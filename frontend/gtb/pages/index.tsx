fc42a3b6d4effe7dcf141af2e206a8977d9fb74f
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2022-01-30 17:26:31 -0400

    fe: link up with AWS cognito

--- frontend/gtb/pages/index.tsx
+++ frontend/gtb/pages/index.tsx
@@ -1,10 +1,52 @@
-import type { NextPage } from 'next'
-import Amplify from 'aws-amplify'
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
+import Amplify, { Auth } from 'aws-amplify'
+import { GithubLoginButton } from 'react-social-login-buttons'
+import { useEffect } from 'react'
 
-const Home: NextPage = () => {
+type Props = {
+  amplifyConfig: object
+}
+
+const Home: NextPage<Props> = ({
+  amplifyConfig
+}: Props) => {
+
+  useEffect(() => {
+    Amplify.configure(amplifyConfig)
+  }, [amplifyConfig])
+
   return (
-    <h1> Hello </h1>
+    <div>
+      <GithubLoginButton
+        onClick={() => {
+          Auth.federatedSignIn({ customProvider: 'Github' })
+        }}
+      />
+    </div>
   )
+}
+
+export const getStaticProps: GetStaticProps<Props> = async (context) => {
+  const amplifyConfig = {
+    Auth: {
+      region: process.env.REACT_APP_AWS_REGION,
+      userPoolId: process.env.REACT_APP_AWS_COGNITO_USER_POOL_ID,
+      userPoolWebClientId: process.env.REACT_APP_AWS_COGNITO_CLIENT_ID,
+      mandatorySignIn: true,
+      oauth: {
+        domain: process.env.REACT_APP_AWS_COGNITO_DOMAIN, 
+        scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
+        redirectSignIn: process.env.REACT_APP_REDIRECT_SIGN_IN,
+        redirectSignOut: process.env.REACT_APP_REDIRECT_SIGN_OUT,
+      }
+    }
+  }
+  
+  return {
+    props: {
+      amplifyConfig
+    }
+  }
 }
 
 export default Home
