2d3666a89bd401269a57b983d778fa3deab91434
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2022-02-09 20:51:42 -0400

    cc: clean up formatting and add comments

--- code-collab/src/server.ts
+++ code-collab/src/server.ts
@@ -10,20 +10,13 @@ const WS_PORT = 3000;
 const redisClient = redis.createClient({
   url: REDIS_SERVER
 })
-
 export type RedisClientType = typeof redisClient
-
-const subscriber = redisClient.duplicate()
-subscriber.connect()
-subscriber.subscribe("test", (message) => {
-  console.log(message);
-})
 
 // Intialize dependencies
 const codeService = createCodeService(redisClient)
-
 const joinRoomController = createJoinRoomController(codeService)
 
+// Initalize server
 const server = new WebSocketServer({ port: WS_PORT })
 server.on(WS_CONNECTION, async (ws) => {
   ws.on(WS_JOIN_ROOM_REQ, (msg) => {
