556165526c2e063d781fb68f4496495f49cfb916
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2022-03-21 16:26:56 +0800

    vc: setup socketio server

---
+++ video-calling/index.js
@@ -0,0 +1,37 @@
+import "dotenv/config"
+import express from "express";
+import { createServer } from "http"
+import { Server } from "socket.io";
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+})
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id)
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callended")
+  })
+
+  socket.on("calluser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("calluser", {
+      signal: signalData,
+      from,
+      name
+    })
+  })
+
+  socket.on("answercall", (data) => {
+    io.to(data.to).emit(callaccepted, data.signal)
+  })
+})
+
+const PORT = process.env.PORT || 5002
+
+server.listen(PORT, () => console.log(`Video server listening on port ${PORT}`))
