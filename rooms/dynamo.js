5ae4eba90566b7dc7efa7da4a649da562dd0f7ec
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2022-03-23 22:58:34 +0800

    rm: complete room service endpoints

--- rooms/dynamo.js
+++ rooms/dynamo.js
@@ -1,10 +1,11 @@
 import AWS from "aws-sdk"
+import { v4 as uuidv4 } from "uuid"
 
 const AWS_REGION = process.env.AWS_REGION
 const DYNAMO_ENDPOINT = process.env.DYNAMO_ENDPOINT
 const serviceConfiguration = {
   region: AWS_REGION,
-  endpoint: AWS_ENDPOINT
+  endpoint: DYNAMO_ENDPOINT
 }
 
 const dynamoDB = new AWS.DynamoDB(serviceConfiguration)
@@ -12,3 +13,76 @@ const dynamoClient = new AWS.DynamoDB.DocumentClient({
   ...serviceConfiguration,
   convertEmptyValues: true
 })
+
+const TABLE_NAME = "Rooms"
+const tableParams = {
+  TableName: TABLE_NAME,
+  KeySchema: [{ AttributeName: "roomCode", KeyType: "HASH" }],
+  AttributeDefinitions: [{ AttributeName: "roomCode", AttributeType: "S" }],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 10,
+    WriteCapacityUnits: 10
+  }
+}
+
+export function initalizeDynamoDB() {
+  if (process.env.NODE_ENV !== "PRODUCTION") {
+    return createTable()
+  }
+}
+
+function createTable() {
+  return dynamoDB
+    .createTable(tableParams)
+    .promise()
+    .then((data) => {
+      console.log(
+        `created table, table description: ${JSON.stringify(data, null, 2)}`
+      )
+    })
+    .catch((err) => {
+      console.error(
+        `unable to create table, error: ${JSON.stringify(err, null, 2)}`
+      )
+    })
+}
+
+export async function getRoom(roomCode) {
+  const params = {
+    TableName: TABLE_NAME,
+    Key: {
+      roomCode
+    }
+  }
+
+  return dynamoClient
+    .get(params)
+    .promise()
+    .then((result) => {
+      return result.Item
+    })
+    .catch((err) => {
+      console.error(`failed to get room: ${err}`)
+      return null
+    })
+}
+
+export async function createRoom() {
+  const newRoomCode = uuidv4()
+  const params = {
+    TableName: TABLE_NAME,
+    Item: {
+      roomCode: newRoomCode
+    }
+  }
+
+  return dynamoClient
+    .put(params)
+    .promise()
+    .then(() => {
+      return newRoomCode
+    })
+    .catch(() => {
+      return ""
+    })
+}
--- rooms/index.js
+++ rooms/index.js
@@ -1,4 +1,6 @@
+import "dotenv/config"
 import express from "express"
+import { createRoom, getRoom, initalizeDynamoDB } from "./dynamo.js"
 
 const PORT = process.env.PORT || 5002
 const app = express()
@@ -7,4 +9,40 @@ app.get("/", (req, res) => {
   res.send("HI")
 })
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.get("/room/:roomCode", (req, res) => {
+  const roomCode = req.params.roomCode
+  const room = getRoom
+
+  if (room) {
+    res.status(200).json({
+      success: true,
+      roomExists: true,
+      roomCode
+    })
+  } else {
+    res.status(200).json({
+      success: true,
+      roomExists: false,
+      roomCode: ""
+    })
+  }
+})
+
+app.post("/room", async (req, res) => {
+  const newRoomCode = await createRoom()
+  if (!newRoomCode) {
+    res.status(500).json({
+      success: false,
+      errorMsg: "Could not create a new room, try again later"
+    })
+  } else {
+    res.status(200).json({
+      success: true,
+      roomCode: newRoomCode
+    })
+  }
+})
+
+initalizeDynamoDB().then(() => {
+  app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+})
